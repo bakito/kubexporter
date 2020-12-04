977b592218b53a51154251079201850cb451f072
bakito <github@bakito.ch>
2020-12-04 21:41:36 +0100

    map asLists

--- pkg/types/config.go
+++ pkg/types/config.go
@@ -4,77 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"path/filepath"
-	"regexp"
-	"strconv"
-	"strings"
-	"text/template"
-	"time"
-
 	"github.com/Masterminds/sprig"
 	"github.com/ghodss/yaml"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"path/filepath"
+	"text/template"
 )
-
-var (
-	invalidFileChars = regexp.MustCompile(`[^a-zA-Z0-9.\-]`)
-)
-
-// GroupResource group resource information
-type GroupResource struct {
-	APIGroup        string
-	APIGroupVersion string
-	APIResource     metav1.APIResource
-	APIVersion      string
-	Instances       int
-	Error           string
-	QueryDuration   time.Duration
-	ExportDuration  time.Duration
-}
-
-// Report generate report rows
-func (r GroupResource) Report(withError bool) []string {
-	row := []string{
-		r.APIGroup,
-		r.APIVersion,
-		r.APIResource.Kind,
-		strconv.FormatBool(r.APIResource.Namespaced),
-		strconv.Itoa(r.Instances),
-		r.QueryDuration.String(),
-		r.ExportDuration.String(),
-	}
-	if withError {
-		row = append(row, r.Error)
-	}
-	return row
-}
-
-// GroupKind get concatenated group and kind
-func (r GroupResource) GroupKind() string {
-	if r.APIGroup != "" {
-		return fmt.Sprintf("%s.%s", r.APIGroup, r.APIResource.Kind)
-	}
-	return r.APIResource.Kind
-}
-
-// Kind get the kind
-func (r GroupResource) Kind() string {
-	return r.APIResource.Kind
-}
-
-// Sort sort GroupResource
-func Sort(resources []*GroupResource) func(int, int) bool {
-	return func(i, j int) bool {
-		ret := strings.Compare(resources[i].APIGroup, resources[j].APIGroup)
-		if ret > 0 {
-			return false
-		} else if ret == 0 {
-			return strings.Compare(resources[i].APIResource.Kind, resources[j].APIResource.Kind) < 0
-		}
-		return true
-	}
-}
 
 // Config export config
 type Config struct {
@@ -189,18 +124,6 @@ func (c *Config) Marshal(us interface{}) ([]byte, error) {
 func (c *Config) Printf(format string, a ...interface{}) {
 	if !c.Quiet {
 		fmt.Printf(format, a...)
-	}
-}
-
-// FilterFields filter fields for a given resource
-func (e *Excluded) FilterFields(res *GroupResource, us unstructured.Unstructured) {
-	for _, f := range e.Fields {
-		unstructured.RemoveNestedField(us.Object, f...)
-	}
-	if e.KindFields != nil && e.KindFields[res.GroupKind()] != nil {
-		for _, f := range e.KindFields[res.GroupKind()] {
-			unstructured.RemoveNestedField(us.Object, f...)
-		}
 	}
 }
 
