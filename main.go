e92775c4797167a30488677ec74bf8720b77852c
bakito <github@bakito.ch>
2023-05-17 15:14:02 +0200

    import Kubernetes client auth plugins

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/bakito/kubexporter/cmd"
+import (
+	"github.com/bakito/kubexporter/cmd"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
 
 func main() {
 	cmd.Execute()
