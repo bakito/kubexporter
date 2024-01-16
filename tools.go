f89d9c2fbca05ce392af77cd77e13b0e7e56efb0
Marc Brugger <github@bakito.ch>
2024-01-16 17:11:42 +0100

    switch to go.uber.org/mock (#358)
    * codeql
    
    * switch to go.uber.org/mock

--- tools.go
+++ tools.go
@@ -5,7 +5,7 @@ package tools
 
 import (
 	_ "github.com/bakito/semver"
-	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
+	_ "go.uber.org/mock/mockgen"
 )
