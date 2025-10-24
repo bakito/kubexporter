a9c7d1fcee186d6dfc6b3848db1755d091587d78
bakito <github@bakito.ch>
2025-10-24 21:21:16 +0200

    update tools

--- .toolbox.mk
+++ .toolbox.mk
@@ -19,37 +19,37 @@ TB_SEMVER ?= $(TB_LOCALBIN)/semver
 TB_GOLANGCI_LINT_VERSION ?= v2.5.0
 # renovate: packageName=github.com/goreleaser/goreleaser/v2
 TB_GORELEASER_VERSION ?= v2.12.7
-# renovate: packageName=go.uber.org/mock/mockgen
+# renovate: packageName=github.com/uber-go/mock
 TB_MOCKGEN_VERSION ?= v0.6.0
 # renovate: packageName=github.com/bakito/semver
 TB_SEMVER_VERSION ?= v1.1.7
 
 ## Tool Installer
 .PHONY: tb.golangci-lint
-tb.golangci-lint: $(TB_GOLANGCI_LINT) ## Download golangci-lint locally if necessary.
-$(TB_GOLANGCI_LINT): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/golangci-lint || GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
+tb.golangci-lint: ## Download golangci-lint locally if necessary.
+	@test -s $(TB_GOLANGCI_LINT) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
 .PHONY: tb.goreleaser
-tb.goreleaser: $(TB_GORELEASER) ## Download goreleaser locally if necessary.
-$(TB_GORELEASER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/goreleaser || GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
+tb.goreleaser: ## Download goreleaser locally if necessary.
+	@test -s $(TB_GORELEASER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
 .PHONY: tb.mockgen
-tb.mockgen: $(TB_MOCKGEN) ## Download mockgen locally if necessary.
-$(TB_MOCKGEN): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/mockgen || GOBIN=$(TB_LOCALBIN) go install go.uber.org/mock/mockgen@$(TB_MOCKGEN_VERSION)
+tb.mockgen: ## Download mockgen locally if necessary.
+	@test -s $(TB_MOCKGEN) || \
+		GOBIN=$(TB_LOCALBIN) go install go.uber.org/mock/mockgen@$(TB_MOCKGEN_VERSION)
 .PHONY: tb.semver
-tb.semver: $(TB_SEMVER) ## Download semver locally if necessary.
-$(TB_SEMVER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/semver || GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
+tb.semver: ## Download semver locally if necessary.
+	@test -s $(TB_SEMVER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
 
 ## Reset Tools
 .PHONY: tb.reset
 tb.reset:
 	@rm -f \
-		$(TB_LOCALBIN)/golangci-lint \
-		$(TB_LOCALBIN)/goreleaser \
-		$(TB_LOCALBIN)/mockgen \
-		$(TB_LOCALBIN)/semver
+		$(TB_GOLANGCI_LINT) \
+		$(TB_GORELEASER) \
+		$(TB_MOCKGEN) \
+		$(TB_SEMVER)
 
 ## Update Tools
 .PHONY: tb.update
